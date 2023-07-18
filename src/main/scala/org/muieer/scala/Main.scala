4fd1d4ca2ad3bf715a532de5ac58a35fe226d99c
muieer <yang_for_work@qq.com>
2023-07-18 20:35:29 +0800

    up

--- src/main/scala/org/muieer/scala/Main.scala
+++ src/main/scala/org/muieer/scala/Main.scala
@@ -1,8 +1,23 @@
 package org.muieer.scala
 
+import org.apache.spark.sql.SparkSession
+
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("hello")
+
+    val spark = SparkSession
+      .builder()
+      .master("local[4]")
+      .appName("spark_memory_dynamic_allocation")
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+    val count = sc.textFile(args(0)).count()
+
+    Thread.sleep(100000)
+    println(count)
+
   }
 }
